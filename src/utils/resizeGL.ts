c21a31699643e8ea085a10133ccb0741a4f90284
Andi Rosca <andi23rosca@gmail.com>
2020-01-31 10:01:12 +0100

    Added check for canvas to support headless gl

--- src/utils/resizeGL.ts
+++ src/utils/resizeGL.ts
@@ -2,10 +2,14 @@ export default function(gl: WebGLRenderingContext, size: [number, number]) {
   const [width, height] = size;
   const displayWidth = Math.floor(width);
   const displayHeight = Math.floor(height);
-
-  if (gl.canvas.width !== displayWidth || gl.canvas.height !== displayHeight) {
-    gl.canvas.width = displayWidth;
-    gl.canvas.height = displayHeight;
+  if (gl.canvas) {
+    if (
+      gl.canvas.width !== displayWidth ||
+      gl.canvas.height !== displayHeight
+    ) {
+      gl.canvas.width = displayWidth;
+      gl.canvas.height = displayHeight;
+    }
   }
 
   gl.viewport(0, 0, ...size);
